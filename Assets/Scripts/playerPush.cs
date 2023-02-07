4e86339d38cdd05ebdf8c3b0bd1a2bcda6b352fa
ConnerMuffin <connermoffet06@gmail.com>
2023-02-07 15:11:01 -0800

    phyic

---
+++ Assets/Scripts/playerPush.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerPush : MonoBehaviour
+{
+    [SerializeField]
+    private float forceMagnitude;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody rigidbody = hit.collider.attachedRigidbody;
+
+        if(rigidbody != null)
+        {
+            Vector3 forceDirection = hit.gameObject.transform.position - transform.position;
+            forceDirection.y = 0;
+            forceDirection.Normalize();
+
+            rigidbody.AddForceAtPosition(forceDirection * forceMagnitude, transform.position, ForceMode.Impulse);
+        }
+    }
+}
