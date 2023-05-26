493495f076bf61c1fd582f899c30de566e38b70e
ConnerMuffin <connermoffet06@gmail.com>
2023-05-26 15:10:55 -0700

    Screw

--- Assets/Scripts/venting.cs
+++ Assets/Scripts/venting.cs
@@ -4,26 +4,65 @@ using UnityEngine;
 
 public class venting : MonoBehaviour
 {
-    public Transform player;
     public LayerMask vent;
     public Transform teleLocIn;
     public Transform teleLocOut;
     public Transform playerView;
     RaycastHit hit;
     bool into = true;
+    public GameObject ventHand;
+    public GameObject unScrewHand;
+    bool unScrewed = false;
+    bool holdingScrewdriver;
+    public pickUpObjects PickUpObjects;
+    public GameObject screwdriver;
+    public LayerMask ventCover;
     
     // Update is called once per frame
     void Update()
     {
+        
         if(Physics.Raycast(playerView.position, playerView.forward, out hit, 2.1f, vent) && into && Input.GetButtonDown("Interact"))
         {
-            player.position = teleLocIn.position;
+            Debug.Log("vented in");
+            transform.position = teleLocIn.position;
             into = false;
         }
         if(Physics.Raycast(playerView.position, playerView.forward, out hit, 2.1f, vent) && !into && Input.GetButtonDown("Interact"))
         {
-            player.position = teleLocOut.position;
+            Debug.Log("vented out");
+            transform.position = teleLocOut.position;
             into = true;
+        }
+        if(Physics.Raycast(playerView.position, playerView.forward, out hit, 2.1f, vent) && unScrewed)
+        {
+            ventHand.SetActive(true);
+        }
+        else
+        {
+            ventHand.SetActive(false);
+        }
+        if(Physics.Raycast(playerView.position, playerView.forward, out hit, 2.1f, ventCover) && !unScrewed && Input.GetButtonDown("Interact") && holdingScrewdriver)
+        {
+            hit.collider.gameObject.SetActive(false);
+        }
+        //Check if holding screwdriver
+        if(PickUpObjects.heldItem == screwdriver)
+        {
+            holdingScrewdriver = true;
+        }
+        else
+        {
+            holdingScrewdriver = false;
+        }
+        //Turn Unscrew text on when look at unscrewed vent
+        if(Physics.Raycast(playerView.position, playerView.forward, out hit, 2.1f, ventCover) && !unScrewed)
+        {
+            unScrewHand.SetActive(true);
+        }
+        else
+        {
+            unScrewHand.SetActive(false);
         }
     }
 }
