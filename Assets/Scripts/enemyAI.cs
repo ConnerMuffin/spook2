c74b66646f6e1c672d15648256d118962e8a1b3f
ConnerMuffin <connermoffet06@gmail.com>
2023-05-29 16:26:55 -0700

    AI
    sight

---
+++ Assets/Scripts/enemyAI.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class enemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+    public Transform gabe;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    public GameObject projectile;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+    public bool canSeePlayer;
+
+    //Sight
+    RaycastHit hit;
+    float maxRange = 20;
+
+    private void Awake()
+    {
+        playerInAttackRange = GameObject.Find("bean_Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        if (Vector3.Distance(transform.position, player.position) < maxRange)
+        {
+            if (Physics.Raycast(transform.position, (player.position - transform.position), out hit, maxRange))
+            {
+                if (hit.transform == player)
+                {
+                    canSeePlayer = true;
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+            }
+        }
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange && canSeePlayer) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange && canSeePlayer) AttackPlayer();
+    }
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random pointin range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+        transform.LookAt(player);
+    }
+    private void AttackPlayer()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //Visualize Attack + Sight Range
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+}
+
